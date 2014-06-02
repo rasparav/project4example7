133f7fef2827cae210308c68655c64c2e27d6851
samer.abdelkafi@gmail.com <samer.abdelkafi@gmail.com@beceda0c-99ed-8924-ea42-6a09b5a7c878>
2014-06-02 23:00:17 +0000

    delete

--- JpaProject/src/main/java/com/mycompany/myproject/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.myproject.service;
-
-import com.mycompany.myproject.persist.entity.User;
-import com.mycompany.myproject.service.dto.UserDto;
-
-public interface UserService extends GenericService<User, UserDto, Long>{
-
-}
